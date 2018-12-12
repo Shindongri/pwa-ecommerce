8d4f3427a7b0b3b64328fb2a9ae2c0f61defbbcc
신동리 <tlsehdfl@naver.com>
2018-12-13 01:21:04 +0900

    # done ( #4-3 )

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import { Col, Icon, Card } from 'antd';
 
+const Text = ( { text } ) => <span style={ { color: 'black' } }>{ text }</span>;
+
 export default ( { id, name, description, photoUrl } ) => (
     <Col span={ 4 } style={ { marginBottom: '25px', width: '200px' } }>
         <Link href={`/product?id=${ id }`}
@@ -11,19 +13,13 @@ export default ( { id, name, description, photoUrl } ) => (
                 style={ { width: '100%' } }
                 actions={ [
                     <a>
-                        <Icon type="eye"
-                              theme="outlined"/>
+                        <Icon type="eye" theme="outlined"/>
                     </a>
                 ] }
-                cover={
-                    <img
-                        alt="example"
-                        src={ photoUrl }
-                        style={ { height: '200px' } }
-                    />
+                cover={ <img alt="example" src={ photoUrl } />
                 }
             >
-                <Card.Meta title={ name } description={ description } />
+                <Card.Meta title={ <Text text={ name } /> } description={ <Text text={ description } /> } />
             </Card>
         </Link>
     </Col>
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Document, {Head, Main, NextScript} from 'next/document';
 
+// PWA ? Manefest 를 얻을 수 있는 브라우져
+// Manefest ? 브라우져에게 어플리케이션의 정보를 알려줌
+
+
+// <meta name="viewport" content="width=device-width, initial-scale=1" /> 웹 사이트가 어디에 보여지고 있는
+// <meta name="description" content="PWA Store" /> 검색창 타이틀 밑에 노출되는 description
+
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx);
@@ -9,11 +16,15 @@ export default class MyDocument extends Document {
 
     render() {
         return (
-            <html>
+            <html lang="en">
             <Head>
                 <meta name="author" content="Dong-ri, Shin" />
-                <link href="//cdnjs.cloudflare.com/ajax/libs/antd/3.8.1/antd.min.css" rel="stylesheet" />
+                <link rel="manifest" href="/static/manifest.json" />
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.8.1/antd.min.css" rel="stylesheet" />
                 <style>{` body { background-color: #EFF2F5!important }`}</style>
+                <meta name="theme-color" content="black" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="E-Commerce with PWA" />
             </Head>
             <body>
             <Main/>
